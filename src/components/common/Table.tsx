92d08a0a13459d2277f64c166cddf622013abb3d
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-10-06 12:55:08 +0300

    add table component

---
+++ src/components/common/Table.tsx
@@ -0,0 +1,141 @@
+import { LandingPage } from "@prisma/client";
+import {
+  ColumnHelper,
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { FC, useMemo } from "react";
+
+interface ITableProps {
+  data: LandingPage[];
+}
+
+const Table: FC<ITableProps> = ({ data }) => {
+  const columnHelper = useMemo<ColumnHelper<LandingPage>>(
+    () => createColumnHelper<LandingPage>(),
+    []
+  );
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor("id", {
+        header: "ID",
+        cell: (row) => row.getValue(),
+      }),
+      columnHelper.accessor("name", {
+        header: "Name",
+        cell: (row) => row.getValue(),
+      }),
+      columnHelper.accessor("id", {
+        header: "Visits",
+        cell: () => 0,
+        id: "visits",
+      }),
+      columnHelper.accessor("id", {
+        header: "Clicks",
+        cell: () => 0,
+        id: "clicks",
+      }),
+      columnHelper.accessor("id", {
+        header: "Leads",
+        cell: () => 0,
+        id: "leads",
+      }),
+      columnHelper.accessor("id", {
+        header: "Cost",
+        cell: () => `$0.00`,
+        id: "cost",
+      }),
+      columnHelper.accessor("id", {
+        header: "Revenue",
+        cell: () => `$0.00`,
+        id: "revenue",
+      }),
+      columnHelper.accessor("id", {
+        header: "Profit",
+        cell: () => `$0.00`,
+        id: "profit",
+      }),
+      columnHelper.accessor("id", {
+        header: "ROI",
+        cell: () => `0.00%`,
+        id: "roi",
+      }),
+      columnHelper.accessor("id", {
+        header: "CTR",
+        cell: () => `0.00%`,
+        id: "ctr",
+      }),
+      columnHelper.accessor("id", {
+        header: "CV",
+        cell: () => `0.00%`,
+        id: "cv",
+      }),
+      columnHelper.accessor("id", {
+        header: "CR",
+        cell: () => `0.00%`,
+        id: "cr",
+      }),
+      columnHelper.accessor("id", {
+        header: "CPV",
+        cell: () => `$0.00`,
+        id: "cpv",
+      }),
+      columnHelper.accessor("id", {
+        header: "EPV",
+        cell: () => `$0.00`,
+        id: "epv",
+      }),
+      columnHelper.accessor("id", {
+        header: "EPC",
+        cell: () => `$0.00`,
+        id: "epc",
+      }),
+    ],
+    [columnHelper]
+  );
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <table className="w-full">
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id} className="bg-slate-200">
+            {headerGroup.headers.map((header) => (
+              <th
+                key={header.id}
+                className="p-2 underline decoration-dashed underline-offset-4 cursor-pointer hover:text-slate-500"
+              >
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id} className="hover:bg-slate-100 cursor-pointer">
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id} className="p-2 text-center text-gray-500">
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,81 +1,11 @@
 import { LandingPage } from "@prisma/client";
-import {
-  createColumnHelper,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
 import Head from "next/head";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Layout from "../../components/common/Layout";
+import Table from "../../components/common/Table";
 import { ProtectedPage } from "../../types/auth-required";
 import { trpc } from "../../utils/trpc";
-
-const columnHelper = createColumnHelper<LandingPage>();
-
-const defaultColumns = [
-  columnHelper.accessor("id", {
-    header: "ID",
-    cell: (row) => row.getValue(),
-  }),
-  columnHelper.accessor("name", {
-    header: "Name",
-    cell: (row) => row.getValue(),
-  }),
-  columnHelper.accessor("id", {
-    header: "Visits",
-    cell: () => 0,
-  }),
-  columnHelper.accessor("id", {
-    header: "Clicks",
-    cell: () => 0,
-  }),
-  columnHelper.accessor("id", {
-    header: "Leads",
-    cell: () => 0,
-  }),
-  columnHelper.accessor("id", {
-    header: "Cost",
-    cell: () => `$0.00`,
-  }),
-  columnHelper.accessor("id", {
-    header: "Revenue",
-    cell: () => `$0.00`,
-  }),
-  columnHelper.accessor("id", {
-    header: "Profit",
-    cell: () => `$0.00`,
-  }),
-  columnHelper.accessor("id", {
-    header: "ROI",
-    cell: () => `0.00%`,
-  }),
-  columnHelper.accessor("id", {
-    header: "CTR",
-    cell: () => `0.00%`,
-  }),
-  columnHelper.accessor("id", {
-    header: "CV",
-    cell: () => `0.00%`,
-  }),
-  columnHelper.accessor("id", {
-    header: "CR",
-    cell: () => `0.00%`,
-  }),
-  columnHelper.accessor("id", {
-    header: "CPV",
-    cell: () => `$0.00`,
-  }),
-  columnHelper.accessor("id", {
-    header: "EPV",
-    cell: () => `$0.00`,
-  }),
-  columnHelper.accessor("id", {
-    header: "EPC",
-    cell: () => `$0.00`,
-  }),
-];
 
 const IndexLandingPage: ProtectedPage = () => {
   const [data, setData] = useState<LandingPage[]>([]);
@@ -84,12 +14,6 @@ const IndexLandingPage: ProtectedPage = () => {
   useEffect(() => {
     if (landingPages.data) setData(landingPages.data);
   }, [landingPages.data]);
-
-  const table = useReactTable({
-    data,
-    columns: defaultColumns,
-    getCoreRowModel: getCoreRowModel(),
-  });
 
   return (
     <>
@@ -104,42 +28,7 @@ const IndexLandingPage: ProtectedPage = () => {
         </Link>
 
         <div className="flex w-full p-2">
-          <table className="w-full">
-            <thead>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <tr key={headerGroup.id} className="bg-slate-200">
-                  {headerGroup.headers.map((header) => (
-                    <th
-                      key={header.id}
-                      className="p-2 underline decoration-dashed underline-offset-4 cursor-pointer hover:text-slate-500"
-                    >
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </th>
-                  ))}
-                </tr>
-              ))}
-            </thead>
-
-            <tbody>
-              {table.getRowModel().rows.map((row) => (
-                <tr key={row.id} className="hover:bg-slate-100 cursor-pointer">
-                  {row.getVisibleCells().map((cell) => (
-                    <td key={cell.id} className="p-2 text-center text-gray-500">
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <Table data={data} />
         </div>
       </div>
     </>
