db2f419ea60cf3c3079cb4a18318aaa2c403c9f6
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-10-07 18:09:42 +0300

    update new landing page

--- src/pages/landing/new.tsx
+++ src/pages/landing/new.tsx
@@ -1,13 +1,10 @@
 import Head from "next/head";
-import { getCodeList } from "country-list";
-import Select from "react-select";
 import { commonLayout } from "../../components/common/Layout";
 import { ProtectedPage } from "../../types/auth-required";
-import Link from "next/link";
-import { SubmitHandler, useForm, Controller } from "react-hook-form";
-import ErrorInputMessage from "../../components/form/ErrorInputMessage";
+import { SubmitHandler } from "react-hook-form";
 import { trpc } from "../../utils/trpc";
 import { useRouter } from "next/router";
+import LandingForm from "../../components/form/LandingForm";
 
 type Inputs = {
   name: string;
@@ -17,12 +14,6 @@ type Inputs = {
 };
 
 const NewLandingPage: ProtectedPage = () => {
-  const {
-    control,
-    register,
-    handleSubmit: onSubmit,
-    formState: { errors },
-  } = useForm<Inputs>();
   const createLandingPage = trpc.useMutation("landingPages.create");
   const router = useRouter();
 
@@ -46,87 +37,7 @@ const NewLandingPage: ProtectedPage = () => {
 
       <div className="p-4">
         <h1 className="text-2xl">New Landing Page</h1>
-        <form className="form" onSubmit={onSubmit(handleSubmit)}>
-          <div className="form-controls">
-            <label htmlFor="name" className="form-control-left">
-              Name:
-            </label>
-            <div className="flex flex-col">
-              <input
-                type="text"
-                id="name"
-                className={`form-control ${
-                  errors.name ? "border-red-500" : ""
-                }`}
-                {...register("name", { required: true, maxLength: 80 })}
-              />
-              {errors.url && (
-                <ErrorInputMessage>
-                  Landing page name is required and the max length is 80 symbols
-                </ErrorInputMessage>
-              )}
-            </div>
-          </div>
-          <div className="form-controls">
-            <label htmlFor="countries" className="form-control-left">
-              Countries:
-            </label>
-            <Controller
-              name="countries"
-              control={control}
-              render={({ field }) => (
-                <Select
-                  {...field}
-                  isMulti
-                  options={Object.entries(getCodeList()).map(
-                    ([code, name]) => ({
-                      value: code,
-                      label: `${name} [${code.toUpperCase()}]`,
-                    })
-                  )}
-                  id="countries"
-                />
-              )}
-            />
-          </div>
-          <div className="form-controls">
-            <label htmlFor="offersCount" className="form-control-left">
-              Offers count:
-            </label>
-            <input
-              type="number"
-              id="offersCount"
-              className="form-control"
-              {...register("offersCount", { required: false })}
-            />
-          </div>
-          <div className="form-controls">
-            <label htmlFor="url" className="form-control-left">
-              URL:
-            </label>
-            <div className="flex flex-col">
-              <input
-                type="text"
-                id="url"
-                className={`form-control ${errors.url ? "border-red-500" : ""}`}
-                {...register("url", { required: true, maxLength: 200 })}
-              />
-              {errors.url && (
-                <ErrorInputMessage>
-                  URL is required and the max length is 200 symbols
-                </ErrorInputMessage>
-              )}
-            </div>
-          </div>
-          <div className="form-controls">
-            <Link href="/landing">
-              <a className="form-control-left button">Cancel</a>
-            </Link>
-            <button type="submit" className="button button-primary">
-              Create
-            </button>
-          </div>
-        </form>
+        <LandingForm onSubmit={handleSubmit} />
       </div>
     </>
   );
