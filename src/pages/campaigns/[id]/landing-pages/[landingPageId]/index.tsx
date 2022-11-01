0f70f70350e5583ca0c4fd795d7aa865034ff960
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-01 16:56:17 +0200

    process visit

--- src/pages/campaigns/[id]/landing-pages/[landingPageId]/index.tsx
+++ src/pages/campaigns/[id]/landing-pages/[landingPageId]/index.tsx
@@ -1,11 +1,15 @@
 import { LandingPage } from "@prisma/client";
 import { GetServerSideProps } from "next";
+import platform from "platform";
 
 const ShowLandingPagePage = () => {
   return <div>Loading...</div>;
 };
 
 export default ShowLandingPagePage;
+
+const LOCAL_IP = "::1";
+const FAKE_IP = "134.201.250.155";
 
 export const getServerSideProps: GetServerSideProps<
   { landingPage: LandingPage },
@@ -36,6 +40,32 @@ export const getServerSideProps: GetServerSideProps<
 
   if (!campaign) return { notFound: true };
   if (!campaign.paths[0]) return { notFound: true };
+
+  const ip =
+    context.req.socket.remoteAddress == LOCAL_IP
+      ? FAKE_IP
+      : context.req.socket.remoteAddress;
+  const response = await fetch(
+    `http://api.ipstack.com/${ip}?access_key=${process.env.IP_STACK_API_KEY}`
+  );
+  const jsonData = await response.json();
+
+  const client = platform.parse(context.req.headers["user-agent"] || "");
+
+  const data = {
+    landingPageId,
+    pathId: campaign.paths[0].id,
+    country: jsonData.country_code,
+    os: client.os?.family || "Unknown",
+    browser: client.name || "Unknown",
+    device: client.product || "Unknown",
+  };
+
+  console.log(ip);
+
+  await prisma?.visit.create({
+    data,
+  });
 
   return {
     redirect: {
