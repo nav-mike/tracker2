f46e7408a1cdf8fbd407260cbbe287a7d1d7e094
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-02 17:35:58 +0200

    add report type

---
+++ src/models/report.ts
@@ -0,0 +1,16 @@
+export type Report = {
+  id: string;
+  name: string;
+  visits: number;
+  clicks: number;
+  cost: number;
+  revenue: number;
+  profit: number;
+  roi: number;
+  ctr: number;
+  cv: number;
+  cr: number;
+  cpv: number;
+  epv: number;
+  epc: number;
+};
