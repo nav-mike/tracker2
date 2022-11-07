67d889f8e6ea809b4a9a0f58814f4221a2316546
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-07 16:16:37 +0200

    redirect to campaigns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import { AuthRequired } from "../types/auth-required";
 import { trpc } from "../utils/trpc";
@@ -90,4 +90,13 @@ const TechnologyCard = ({
       </a>
     </section>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    redirect: {
+      destination: "/campaigns",
+      permanent: false,
+    },
+  };
 };
