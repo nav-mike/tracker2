0cbfa3de5977b31ab9860b4207d09eec16db5ba9
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-14 15:44:42 +0200

    fix navbar bug

--- src/components/users/UserNav.tsx
+++ src/components/users/UserNav.tsx
@@ -1,23 +1,28 @@
 import { signOut } from "next-auth/react";
 import Link from "next/link";
-import { FC, useState } from "react";
+import { FC, useRef, useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
 import { GoChevronDown } from "react-icons/go";
 import { MdLogout } from "react-icons/md";
+import { useOutsideClick } from "../../hooks/useOutsideClick";
 
 const UserNav: FC<{ username: string }> = ({ username }) => {
   const [showMenu, setShowMenu] = useState(false);
+  const navbarRef = useRef<HTMLDivElement>(null);
+
+  useOutsideClick(navbarRef, () => setShowMenu(false));
 
   const clickHandler = () => {
     setShowMenu(!showMenu);
   };
 
   const logoutHandler = () => {
+    setShowMenu(false);
     signOut({ callbackUrl: "/auth/signin" });
   };
 
   return (
-    <>
+    <div ref={navbarRef}>
       <button
         className={`flex flex-row gap-2 items-center p-3 rounded-md ${
           showMenu ? "bg-gray-300" : ""
@@ -29,7 +34,7 @@ const UserNav: FC<{ username: string }> = ({ username }) => {
       {showMenu && (
         <div className="absolute right-4 flex flex-col bg-gray-100 top-12 rounded-b-md p-2">
           <Link href="/billing">
-            <a className="user-menu-item">
+            <a className="user-menu-item" onClick={() => setShowMenu(false)}>
               <AiOutlineUser /> Billing
             </a>
           </Link>
@@ -38,7 +43,7 @@ const UserNav: FC<{ username: string }> = ({ username }) => {
           </button>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
